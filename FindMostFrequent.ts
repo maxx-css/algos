844d3f8b8fa88037352572cfde93642a462c8fe5
Max Rivera <121986363+maxx-css@users.noreply.github.com>
2024-04-21 13:48:52 -0400

    completed findMostFrequent

--- FindMostFrequent.ts
+++ FindMostFrequent.ts
@@ -31,8 +31,80 @@ Questions:
 
 Approach
 -I want to solve for the simplest case first, like what I get an empty array or if I get an array with just one item 
+-go through each item in the array, 
+-for each item, count how many times ive encountered the item and add it to a tally (use case for a hash table)
+--the item is the key and the count is the value
+--store them in the order ive been finding them 
+-once ive looked at all the items in the array, reference the hash table and see which key has the highest number as its value
+-check to see what the next highest number is
+--if the next highest number is the same as the highest, return the first key (do i even need to do that? as long as i store them in order i can always just reference the highest number )
 
 
 
 */
-console.log('hellsaxo')
+const testArray: Array<number> = [3, 7, 3, 3, 2, 2, 2, 2, 1];
+
+// function findMostFrequent(inputArray: number[]) {
+//     const itemCountMap: { [key: number]: number } = {};
+
+//     function keyWithHighestValue(obj: Record<string,number>): string | null{
+//         let highestKey: string | null = null;
+//         let highestValue: number = -Infinity;
+//         for (const key in obj) {
+//           if (obj.hasOwnProperty(key)) {
+//             if (obj[key] > highestValue) {
+//               highestValue = obj[key];
+//               highestKey = key;
+//             }
+//           }
+//         }
+//         return highestKey;
+//     }
+
+//     if (inputArray.length === 0){
+//         return null;
+//     }
+
+//     inputArray.forEach(item => {
+//         if (!itemCountMap[item]){
+//             itemCountMap[item] = 1;
+//         } else {
+//             itemCountMap[item]++;
+//         }
+//     });
+
+//     return keyWithHighestValue(itemCountMap)
+    
+    
+
+// }
+
+// Chads response 
+
+function findMostFrequent(inputArray: number[]): number | null {
+  if (inputArray.length === 0) {
+    return null;
+  }
+
+  const itemCountMap: { [key: number]: number } = {};
+  let mostFrequentItem = inputArray[0];
+  let highestFrequency = 1;
+
+  inputArray.forEach((item) => {
+    if (itemCountMap[item]) {
+      itemCountMap[item]++;
+    } else {
+      itemCountMap[item] = 1;
+    }
+    if (itemCountMap[item] > highestFrequency) {
+      mostFrequentItem = item;
+      highestFrequency = itemCountMap[item];
+    }
+  });
+
+  return mostFrequentItem;
+}
+
+
+console.log('function  return', findMostFrequent(testArray));
+ 
